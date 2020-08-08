f78c1a31a3cc82f2e27c8c92eae283c652f4e2a7
Dilip Sharma <dilipsharma640@gmail.com>
2020-08-08 15:42:41 +0530

    Fix json parse

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, Request
-from typing import Any, Dict
-
-from model import ModelOutput
 
 app = FastAPI()
 
 @app.get("/getXLSX")
-async def root(request: Dict[Any, Any]):
-    # request_body = await request.json()
-    print(request)
+async def root(request: Request):
+    request_body = await request.json()
+    print(request_body['model_output'][0]['bounding_box'])
     return "OK"
